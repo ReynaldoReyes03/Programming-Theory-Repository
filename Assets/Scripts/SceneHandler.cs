f9529668eec54021952dbf2271bd5e4f7c32a0cc
ReynaldoReyes03 <120312433+ReynaldoReyes03@users.noreply.github.com>
2022-12-31 05:24:14 -0500

    Scene handler script creation

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler {
+    public void OpenGameScene() {
+        if (Time.timeScale == 0.0f) Time.timeScale = 1.0f;
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenMenuScene() {
+        if (Time.timeScale == 0.0f) Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+}
