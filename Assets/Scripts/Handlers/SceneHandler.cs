c09e316396d51d97ef5f83663e95e4c390405764
ReynaldoReyes03 <120312433+ReynaldoReyes03@users.noreply.github.com>
2022-12-31 17:51:41 -0500

    Scene Handler script and Game Scene improvements

--- Assets/Scripts/Handlers/SceneHandler.cs
+++ Assets/Scripts/Handlers/SceneHandler.cs
@@ -3,12 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour {
     public void OpenGameScene() {
-        if (Time.timeScale == 0.0f) Time.timeScale = 1.0f;
         SceneManager.LoadScene(1);
     }
 
     public void OpenMenuScene() {
-        if (Time.timeScale == 0.0f) Time.timeScale = 1.0f;
         SceneManager.LoadScene(0);
     }
 }
